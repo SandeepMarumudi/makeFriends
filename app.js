37806a63e11d57a9f72d537de4db8877b68341fb
SandeepMarumudi <marumudisandeep@gmail.com>
2025-08-05 02:01:59 +0530

    middlewares authCheck

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+const {adminAuth}=require("./src/middlewares/admin")
 const express=require("express")
 const app=express()
 
 
+app.use("/admin",adminAuth)
 
-app.use("/serverr",(req,res)=>{
-res.send("server from the server")
+app.get("/admin/login",(req,res)=>{
+res.send("admin is logined")
 })
+
+
+
 
 app.listen(7777,()=>{
     console.log("server started running in 7777")
