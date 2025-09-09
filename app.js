1f49aa04af19d04e58e493de67a129ee7b2a2ca3
SandeepMarumudi <marumudisandeep@gmail.com>
2025-09-09 18:00:08 +0530

    cor

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cookie())
 app.use(express.json());
 app.use(cors({
   origin: "https://make-friends-frontend-two.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials:true
 })) //cors error while fetching api's from the frontEnd
 
