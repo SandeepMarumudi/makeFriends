1c2dabf5bb370712913f15ef7c667612bb0fd7d6
SandeepMarumudi <marumudisandeep@gmail.com>
2025-09-09 21:26:21 +0530

    login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,26 +1,30 @@
-const express=require("express")
-const bcrypt=require("bcrypt")
-const User=require("../models/users")
-const {validateUser} = require("../utils/validations")
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/users");
+const { validateUser } = require("../utils/validations");
 
-
-
-
- 
-
-const authRouter=express.Router()
+const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    const { firstName, lastName, email, phone, age, password, gender,skills,photoUrl,about } =
-      req.body;
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      age,
+      password,
+      gender,
+      skills,
+      photoUrl,
+      about,
+    } = req.body;
 
     //check the user for valide information
     validateUser(req.body);
 
     //EnCrypt the password
     const beCryptPassword = await bcrypt.hash(password, 10);
-  
 
     // after validations and becryption of password insert data to the database
 
@@ -34,46 +38,50 @@ authRouter.post("/signup", async (req, res) => {
       age,
       skills,
       photoUrl,
-      about
+      about,
     });
 
-   const savedUser= await user.save();
-  const token= await savedUser.getJWT()
-    res.cookie("token",token)
-   
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token);
 
-    res.json({message:"User added successfull",data:savedUser});
+    res.json({ message: "User added successfull", data: savedUser });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
-authRouter.post("/login",async(req,res)=>{
-  const {email,password}=req.body
-  try{
-    const user= await User.findOne({email:email})
-    if(!user){
-      throw new Error("Email id not found please SignUp")
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("Email id not found please SignUp");
     }
 
-    const checkPassword= await user.validatePassword(password)
-    if(checkPassword){
-      const token= await user.getJWT()
-      res.cookie("token",token)
-      res.send(user)
-    }else{
-      throw new Error("Password wrong please re enter")
+    const checkPassword = await user.validatePassword(password);
+    if (checkPassword) {
+      const token = await user.getJWT();
+      res
+        .cookie("token", token, {
+          httpOnly: true, // can’t be accessed with JS → safer
+          secure: true, // required on HTTPS (Render uses HTTPS)
+          sameSite: "None", // required when frontend & backend are on different domains
+          path: "/", // make cookie available everywhere
+        })
+        .json(user);
+      res.send(user);
+    } else {
+      throw new Error("Password wrong please re enter");
     }
-
-
-  }catch(err){
-    res.status(400).json({ message: err.message })
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
-authRouter.post("/logout",async(req,res)=>{
-  res.cookie("token",null,{expires:new Date(Date.now())})
-  res.send("successfully logged out")
-})
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, { expires: new Date(Date.now()) });
+  res.send("successfully logged out");
+});
 
-module.exports=authRouter 
+module.exports = authRouter;
