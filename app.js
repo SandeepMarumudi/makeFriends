c71888024822934f5038c9fe8d6d67f8b539064e
SandeepMarumudi <marumudisandeep@gmail.com>
2025-09-09 17:23:49 +0530

    mod

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookie())
 app.use(express.json());
 app.use(cors({
-  origin:"*",
+  origin:[  "http://localhost:5173","https://make-friends-chi.vercel.app"],
   credentials:true
 })) //cors error while fetching api's from the frontEnd
 
